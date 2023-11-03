f40258f81fe6e5f8b0d5b8dcc4d800ec4147e409
Salman-Irfan <salmanirfan674@gmail.com>
2023-11-03 12:20:54 +0500

    setting up react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import "./styles/global.css"
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Route, Routes } from 'react-router-dom'
+import Register from './pages/auth/Register'
+import Login from './pages/auth/Login'
+import Home from './pages/Home'
 const App = () => {
+  
 
   return (
     <>
-      <h1>Hello MERN Chat App</h1>
+      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        {/* auth route */}
+        <Route path='/auth/register' element={<Register/>}/>
+        <Route path='/auth/login' element={<Login/>}/>
+      </Routes>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <Button colorScheme='blue'>Chakra Button</Button>
+            <h1>Hello MERN Chat App</h1>
+            Home
+        </>
+    )
+}
+
+export default Home
